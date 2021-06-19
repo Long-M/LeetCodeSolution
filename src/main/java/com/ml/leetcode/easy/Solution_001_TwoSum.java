c7ab9332cd766e3f52e7121da61086b3bfc182d9
maolong <350723986@qq.com>
2021-06-19 19:14:59 +0800

    提交两数之和题解

---
+++ src/main/java/com/ml/leetcode/easy/Solution_001_TwoSum.java
@@ -0,0 +1,27 @@
+package com.ml.leetcode.easy;
+
+import java.util.HashMap;
+import java.util.Map;
+
+/**
+ * @author Mr.ml
+ * @date 2021/6/19
+ *
+ * 给定一个整数数组nums和一个整数目标值target，请你在该数组中找出和为目标值target的那两个整数，并返回它们的数组下标。
+ * 你可以假设每种输入只会对应一个答案。但是，数组中同一个元素在答案里不能重复出现。
+ * 你可以按任意顺序返回答案。
+ */
+public class Solution_001_TwoSum {
+
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+        for (int i = 0; i < nums.length; i++) {
+            if (map.containsKey(target - nums[i])) {
+                return new int[]{i, map.get(target - nums[i])};
+            }
+            map.put(nums[i], i);
+        }
+        return new int[2];
+    }
+
+}
