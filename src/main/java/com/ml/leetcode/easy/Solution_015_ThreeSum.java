c6c6ffd05e706578f8f256f9f62db12fc2acc2a6
maolong <350723986@qq.com>
2021-06-19 19:32:19 +0800

    提交三数之和题解

---
+++ src/main/java/com/ml/leetcode/easy/Solution_015_ThreeSum.java
@@ -0,0 +1,47 @@
+package com.ml.leetcode.easy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+/**
+ * @author Mr.ml
+ * @date 2021/6/19
+ *
+ * 给你一个包含n个整数的数组nums，判断nums中是否存在三个元素a，b，c，使得a+b+c=0？
+ * 请你找出所有和为0且不重复的三元组。
+ * 注意：答案中不可以包含重复的三元组。
+ */
+public class Solution_015_ThreeSum {
+
+    public List<List<Integer>> threeSum(int[] nums) {
+        if (nums.length == 0) {
+            return new ArrayList<>();
+        }
+        Arrays.sort(nums);
+        List<List<Integer>> res = new ArrayList<>();
+        for (int i = 0; i < nums.length - 2; i++) {
+            // 去重
+            if (i > 0 && nums[i] == nums[i - 1]) {
+                continue;
+            }
+            int left = i + 1;
+            int right = nums.length - 1;
+            int target = -nums[i];
+            while (left < right) {
+                int sum = nums[left] + nums[right];
+                if (sum == target) {
+                    res.add(Arrays.asList(nums[i], nums[left], nums[right]));
+                    while (left < right && nums[left] == nums[++left]);
+                    while (left < right && nums[right] == nums[--right]);
+                } else if (sum < target) {
+                    left++;
+                } else {
+                    right--;
+                }
+            }
+        }
+        return res;
+    }
+
+}
