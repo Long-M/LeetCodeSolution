90a2802abd160f19cb45725f0c70c985ac62b27a
maolong <350723986@qq.com>
2021-07-06 21:34:28 +0800

    提交平衡二叉树题解

---
+++ src/main/java/com/ml/leetcode/easy/Solution_110_BalancedBinaryTree.java
@@ -0,0 +1,47 @@
+package com.ml.leetcode.easy;
+
+/**
+ * @author Mr.ml
+ * @date 2021/7/6
+ *
+ * 给定一个二叉树，判断它是否是高度平衡的二叉树。
+ * 本题中，一棵高度平衡二叉树定义为：
+ * 一个二叉树每个节点 的左右两个子树的高度差的绝对值不超过1。
+ */
+public class Solution_110_BalancedBinaryTree {
+
+    public boolean isBalanced(TreeNode root) {
+        if (root == null) {
+            return true;
+        }
+        // 左右子树高度差小于1，并且左右子树也是平衡二叉树
+        return Math.abs(getDepth(root.left) - getDepth(root.right)) <= 1 && isBalanced(root.left) && isBalanced(root.right);
+    }
+
+    public int getDepth(TreeNode root) {
+        if (root == null) {
+            return 0;
+        }
+        return Math.max(getDepth(root.left), getDepth(root.right)) + 1;
+    }
+
+}
+
+class TreeNode {
+    int val;
+    TreeNode left;
+    TreeNode right;
+
+    TreeNode() {
+    }
+
+    TreeNode(int val) {
+        this.val = val;
+    }
+
+    TreeNode(int val, TreeNode left, TreeNode right) {
+        this.val = val;
+        this.left = left;
+        this.right = right;
+    }
+}
