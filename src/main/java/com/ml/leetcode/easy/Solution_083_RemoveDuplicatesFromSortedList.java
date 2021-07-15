c57e87af4f90a5f77309d816c9b511e338a3e029
maolong <350723986@qq.com>
2021-07-15 23:05:56 +0800

    提交删除排序链表中的重复元素题解

--- src/main/java/com/ml/leetcode/easy/Solution_083_RemoveDuplicatesFromSortedList.java
+++ src/main/java/com/ml/leetcode/easy/Solution_083_RemoveDuplicatesFromSortedList.java
@@ -4,7 +4,7 @@ package com.ml.leetcode.easy;
  * @author Mr.ml
  * @date 2021/7/15
  *
- * 存在一个按升序排列的链表，给你这个链表的头节点 head ，请你删除所有重复的元素，使每个元素 只出现一次 。
+ * 存在一个按升序排列的链表，给你这个链表的头节点head，请你删除所有重复的元素，使每个元素只出现一次。
  * 返回同样按升序排列的结果链表。
  */
 public class Solution_083_RemoveDuplicatesFromSortedList {
