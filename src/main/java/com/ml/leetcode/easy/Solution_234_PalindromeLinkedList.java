da267cdafd960a738d6677914b0c8d7bd022c8ac
maolong <350723986@qq.com>
2021-07-21 23:42:43 +0800

    提交回文链表题解

---
+++ src/main/java/com/ml/leetcode/easy/Solution_234_PalindromeLinkedList.java
@@ -0,0 +1,50 @@
+package com.ml.leetcode.easy;
+
+/**
+ * @author Mr.ml
+ * @date 2021/7/21
+ *
+ * 请判断一个链表是否为回文链表。
+ */
+public class Solution_234_PalindromeLinkedList {
+
+    public boolean isPalindrome(ListNode head) {
+        if (head == null || head.next == null) {
+            return true;
+        }
+        ListNode firstHalfEnd = endOfFirstHalf(head);
+        ListNode secondHalfStart = reverseList(firstHalfEnd.next);
+        ListNode p1 = head;
+        ListNode p2 = secondHalfStart;
+        boolean result = true;
+        while (result && p2 != null) {
+            if (p1.val != p2.val) {
+                result = false;
+            }
+            p1 = p1.next;
+            p2 = p2.next;
+        }
+        return result;
+    }
+
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head, next = null;
+        while (cur != null) {
+            next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+
+    public ListNode endOfFirstHalf(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast.next != null && fast.next.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+}
